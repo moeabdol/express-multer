098655e752fa5f15fec53c72178e9e74b44258e4
moeabdol <mohd.a.saed@gmail.com>
2017-09-07 20:08:55 +0300

    Install morgan for logging

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express    = require("express");
 const bodyParser = require("body-parser");
+const morgan     = require("morgan");
 const app        = express();
 
+app.use(morgan("dev"));
 app.use(bodyParser.json());
 
 app.get("*", (req, res) => {
